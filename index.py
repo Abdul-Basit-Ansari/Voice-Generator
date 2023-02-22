8b2470ca0e795b3b6fcb3db6ebe8782ec90a5326
Abdul-Basit-Ansari <abdulbasitansari288@gmail.com>
2023-02-22 22:38:47 +0500

    add requirments.txt
    Co-authored-by: Abdul-Basit-Ansari <abdulbasitansari288@gmail.com>

--- index.py
+++ index.py
@@ -1,12 +1,14 @@
 # Abdul-Basit-Ansari
 
+# pip install gtts
 from gtts import gTTS
+# pip install playsound
 from playsound import playsound
 
 audio = 'speech.mp3'
 lang = 'en'
-txt = input("enter your text here")
-sp = gTTS(text=txt,lang=lang, tld='com.in',slow=False)
+# txt = input("Enter Your Text Here : ")
+sp = gTTS(text="Enter Your Text Here : ",lang=lang, tld='com.in',slow=False)
 
 sp.save(audio)
 playsound(audio)
